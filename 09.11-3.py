d50d9cdc30e9862da98214041889fef62e8b8fcf
IrinaNizhnikava <nia_1972@mail.ru>
2021-11-14 21:49:22 +0300

    Add renaming in "Задача №3"

--- 09.11-3.py
+++ 09.11-3.py
@@ -2,5 +2,5 @@ def degree(number):
     spis = [number * number * number for _ in range(1)]
     print(spis[0])
 if __name__:
-    number_i = int(input("введите число "))
-    degree(number_i)
+    number = int(input("введите число "))
+    degree(number)
